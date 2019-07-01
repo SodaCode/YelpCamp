8e37af5e05cb1b1a947df02ffff5ffa43e83f1fd
Dominic Saechao <sinchao54@gmail.com>
2019-07-01 07:47:57 +0000

    added the ip gate

--- app.js
+++ app.js
@@ -12,11 +12,8 @@ const commentRoutes     = require("./routes/comments"),
       campgroundRoutes  = require("./routes/campgrounds"),
       indexRoutes       = require("./routes/index");
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
 mongoose.connect("mongodb+srv://sodacode:!Kdh4lyfe@cluster0-3g98e.mongodb.net/test?retryWrites=true&w=majority", {
-    userNewUrlParser: true,
+    useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => {
     console.log("Connected to DB");
